663de07d6c8af5b3fa21324ba1b8184083394207
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-11-03 20:13:24 +0530

    Better looking tests code.

--- spec/task-list.js
+++ spec/task-list.js
@@ -1,22 +1,49 @@
-describe('Task lists spec', function () {
+describe('API endpoint integration', function () {
 
-    it('should create, update and delete a new task list', function () {
-        var flag = false;
+    it('should work with the Google API endpoint flawlessly', function () {
+        var testSequenceTimeout = 10000,
+            initialListCount = 0;
 
+        // The master collection of all the task lists.
+        var taskListsCollection = new TaskListsCollection();
+
+        // The new list instance to play with.
         var newList = new TaskList({
             title: 'New list'
         });
 
+        // Fetch all the lists, before we do anything.
+        var fetchLists1 = function () {
+            taskListsCollection.fetch({
+                success: function (collection, response) {
+                    initialListCount = collection.length;
+                    createList();
+                }
+            });
+        };
+
+        // Create a new list, as defined above.
         var createList = function () {
             newList.save({}, {
                 success: function (model, response) {
                     expect(newList.get('id')).toBeDefined();
                     expect(newList.get('title')).toEqual('New list');
+                    fetchLists2();
+                }
+            });
+        };
+
+        // Fetch all lists again, and see that there is a new one.
+        var fetchLists2 = function () {
+            taskListsCollection.fetch({
+                success: function (collection, response) {
+                    expect(collection.length).toEqual(initialListCount + 1);
                     renameList();
                 }
             });
         };
 
+        // Update the title of the list and confirm its changed.
         var renameList = function () {
             newList.save({title: 'Updated list'}, {
                 success: function (model, response) {
@@ -26,19 +53,36 @@ describe('Task lists spec', function () {
             });
         };
 
+        // Delete the newly created list.
         var deleteList = function () {
             newList.destroy({
                 success: function (model, response) {
                     expect(response).toBeNull();
-                    flag = true;
+                    fetchLists3();
                 }
             });
         };
 
-        runs(createList);
+        // Fetch all lists again, and see that its back to the original listing.
+        var fetchLists3 = function () {
+            taskListsCollection.fetch({
+                success: function (collection, response) {
+                    expect(collection.length).toEqual(initialListCount);
+                    finishTestSequence();
+                }
+            });
+        };
+
+        // Testing is done, let Jasmine know of this.
+        var flag = false, finishTestSequence = function () {
+            flag = true;
+        };
+
+        runs(fetchLists1);
 
         waitsFor(function () { return flag; },
-               'TaskList functionality testing is not stopping.', 5000);
+               'TaskList functionality testing is not stopping.',
+               testSequenceTimeout);
 
     });
 
