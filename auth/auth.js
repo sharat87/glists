2e400d8512939f9db1f1063b31caebabbafa3292
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-11-03 09:44:42 +0530

    Cleanup of auth.js file.

--- auth/auth.js
+++ auth/auth.js
@@ -1,28 +1,34 @@
-var auth;
+(function() {
 
-if (location.hash) {
-  (function() {
-    var auth, param, parts, _i, _len, _ref;
-    auth = {};
-    _ref = location.hash.substr(1).split('&');
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      param = _ref[_i];
-      parts = param.split('=');
-      auth[parts[0]] = parts[1];
+    if (location.hash) {
+
+        var auth = {},
+            props = location.hash.substr(1).split('&'),
+            i = props.length;
+
+        while (i-- > 0) {
+            var parts = props[i].split('=');
+            auth[parts[0]] = parts[1];
+        }
+
+        if (auth.error) {
+            alert('Authentication error: ' + auth.error);
+        } else {
+            auth.verified = false;
+            localStorage.auth = JSON.stringify(auth);
+        }
+
     }
-    if (auth.error != null) {
-      return alert('Authentication error: ' + auth.error);
+
+    if (localStorage.auth && JSON.parse(localStorage.auth)) {
+        window.location = chrome.extension.getURL('index.html');
     } else {
-      auth.verified = false;
-      return localStorage.auth = JSON.stringify(auth);
+        window.location = 'https://accounts.google.com/o/oauth2/auth' +
+        '?response_type=token' +
+        '&client_id=' + CLIENT_ID +
+        '&redirect_uri=' + escape('https://www.google.com/robots.txt') +
+        '&scope=' + escape('https://www.googleapis.com/auth/tasks') +
+        '&state=glists-app-auth';
     }
-  })();
-}
 
-if ((localStorage.auth != null) && ((auth = JSON.parse(localStorage.auth)) != null)) {
-  window.location = chrome.extension.getURL('index.html');
-}
-
-$('#login-btn').on('click', function() {
-  return window.location = 'https://accounts.google.com/o/oauth2/auth' + '?response_type=token' + '&client_id=' + CLIENT_ID + '&redirect_uri=' + escape('https://www.google.com/robots.txt') + '&scope=' + escape('https://www.googleapis.com/auth/tasks') + '&state=glists-app-auth';
-});
+})();
