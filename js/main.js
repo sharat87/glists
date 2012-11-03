ba562476c7be9b301143eba1cedb993416444c1f
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-11-03 11:58:42 +0530

    Clean up main.js file.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,36 @@
-(function () {
+authenticated(function (auth) {
 
-    var startApp = function (auth) {
+    var taskListsCollection = new TaskListsCollection();
 
-        var taskListsCollection = new TaskListsCollection();
+    taskListsCollection.on('reset', function () {
+        new TaskListsCollectionView({
+            collection: this
+        }).renderAndApply();
+    });
 
-        taskListsCollection.on('reset', function () {
-            new TaskListsCollectionView({
-                collection: this
-            }).renderAndApply();
+    taskListsCollection.fetch();
+
+    var newTaskListForm = $('#new-task-list-form'),
+        newListTitle = newTaskListForm.find('input[name=title]');
+    newTaskListForm.on('submit', function (e) {
+        e.preventDefault();
+        var newList = new TaskList({
+            title: newListTitle.val()
         });
-
+        newList.save();
         taskListsCollection.fetch();
+        return newListTitle.val('');
+    });
 
-        var newTaskListForm = $('#new-task-list-form');
-        var newListTitle = newTaskListForm.find('input[name=title]');
-        newTaskListForm.on('submit', function (e) {
-            e.preventDefault();
-            var newList = new TaskList({
-                title: newListTitle.val()
-            });
-            newList.save();
-            taskListsCollection.fetch();
-            return newListTitle.val('');
+    var newTaskForm = $('#new-task-form'),
+        newTaskTitle = newTaskForm.find('input[name=title]');
+    newTaskForm.on('submit', function (e) {
+        e.preventDefault();
+        var newTask = new TaskItem({
+            title: newTaskTitle.val(),
+            tasklist: TaskListView.currentList.get('tasklist')
         });
+        newTask.save();
+    });
 
-        var newTaskForm = $('#new-task-form');
-        var newTaskTitle = newTaskForm.find('input[name=title]');
-        newTaskForm.on('submit', function (e) {
-            e.preventDefault();
-            var newTask = new TaskItem({
-                title: newTaskTitle.val(),
-                tasklist: TaskListView.currentList.get('tasklist')
-            });
-            newTask.save();
-        });
-
-    };
-
-    authenticated(startApp);
-
-}).call(this);
+});
