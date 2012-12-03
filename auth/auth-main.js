6e88efd05befa6fb65a1c0ccd8abed9e1f7ba437
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-12-04 00:06:35 +0530

    Save the absolute time of token expiry.

--- auth/auth-main.js
+++ auth/auth-main.js
@@ -11,6 +11,11 @@
             auth[parts[0]] = parts[1];
         }
 
+        if (auth.expires_in) {
+            auth.expires_in = parseInt(auth.expires_in, 10);
+            auth.expires_at = new Date().valueOf() + auth.expires_in * 1000;
+        }
+
         if (auth.error) {
             alert('Authentication error: ' + auth.error);
         } else {
